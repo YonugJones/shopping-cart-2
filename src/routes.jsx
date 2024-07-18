228dab8aac653122eb28e416d5681b12e8e6cd4f
YonugJones <peterckerfoot@gmail.com>
2024-07-18 11:41:18 -0500

    created inital routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,15 @@
 import App from './App.jsx'
+import Home from './Home/Home.jsx';
+import ErrorPage from './ErrorPage/ErrorPage.jsx';
 
 const routes = [
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> }
+    ]
   }
 ]
 
