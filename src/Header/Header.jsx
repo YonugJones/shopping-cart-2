dd3d639c511e733bb17aacbbb8b32072c8eb7452
YonugJones <peterckerfoot@gmail.com>
2024-07-19 10:20:55 -0500

    updated Header component to include state

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,8 +2,17 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.css';
+import { useState } from 'react';
+import Cart from '../Cart/Cart';
 
-const Header = () => {
+
+const Header = ({ cartItems, removeFromCart, updateQuantity }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  }
+
   return (
     <div className={styles['header-container']}>
       <div className={styles['header-title']}>The Outlet</div>
@@ -14,6 +23,14 @@ const Header = () => {
       <div className={styles['header-checkout']}>
         <FontAwesomeIcon icon={faCartShopping} />
       </div>
+      {isCartOpen && (
+        <Cart 
+          cartItems={cartItems}
+          onClose={toggleCart}
+          removeFromCart={removeFromCart}
+          updateQuantity={updateQuantity}
+        />
+      )}
     </div>
   )
 }
